99d15e88746a1d16faac3de4e3ce9907c487b33c
Dan Rose <dan@digilabs.io>
2019-07-25 13:28:16 -0500

    Simplify GraphicsMagick++ cmake

--- nav2_map_server/cmake_modules/FindGraphicsMagick++.cmake
+++ nav2_map_server/cmake_modules/FindGraphicsMagick++.cmake
@@ -1,35 +1,22 @@
 # CMake script for finding GraphicsMagick++, the C++ interface for the GraphicsMagick library
 #
 # Output variables:
-#  MAGICK++_FOUND        - system has GraphicsMagick++
-#  MAGICK++_INCLUDE_DIRS - include directories for GraphicsMagick
-#  MAGICK++_LIBRARIES    - libraries you need to link to
+#  GraphicsMagick++_FOUND        - system has GraphicsMagick++
+#  GraphicsMagick++_INCLUDE_DIRS - include directories for GraphicsMagick
+#  GraphicsMagick++_LIBRARIES    - libraries you need to link to
+include(FindPackageHandleStandardArgs)
 
-find_path(MAGICK++_INCLUDE_DIR
+find_path(GraphicsMagick++_INCLUDE_DIRS
   NAMES "Magick++.h"
-  PATHS ${GraphicsMagick++_INCLUDE_PATH}
+  PATH_SUFFIXES GraphicsMagick
   )
 
-find_library(MAGICK++_LIBRARY
-  NAMES GraphicsMagick++
-  PATHS ${GraphicsMagick++_LIBRARY_PATH}
+find_library(GraphicsMagick++_LIBRARIES
+  NAMES "GraphicsMagick++"
   )
 
-set(MAGICK++_LIBRARIES ${MAGICK++_LIBRARY})
-set(MAGICK++_INCLUDE_DIRS ${MAGICK++_INCLUDE_DIR})
-
-if(${MAGICK++_INCLUDE_DIR} AND ${MAGICK++_LIBRARIES})
-  set(MAGICK++_FOUND YES)
-else()
-  set(MAGICK++_FOUND NO)
-endif()
-
-if(MAGICK++_FOUND)
-  # make FIND_PACKAGE friendly
-  if(GraphicsMagick++_FIND_REQUIRED)
-    message(FATAL_ERROR
-      "GraphicsMagick was not found")
-  else(Magick_FIND_REQUIRED)
-    message(STATUS "GraphicsMagick was not found.")
-  endif(GraphicsMagick++_FIND_REQUIRED)
-endif(MAGICK++_FOUND)
+find_package_handle_standard_args(
+  GraphicsMagick++
+  GraphicsMagick++_LIBRARIES
+  GraphicsMagick++_INCLUDE_DIRS
+)
